a20617e1cab538494eac04c52e93660c9d0d25de
Trifun Savic <trifunsavic@gmail.com>
2022-08-16 12:56:17 +0200

    Cleaning the robot code

--- projects/00std_rrdv_robot/00std_rrdv_robot.c
+++ projects/00std_rrdv_robot/00std_rrdv_robot.c
@@ -185,9 +185,9 @@ int main(void) {
     robot_init_setup();  
 
     while (1) {        
-      /*  __WFE();      
-        __SEV();
-        __WFE();*/
+        //__WFE();      
+        //__SEV();
+        //__WFE();
     }
     // one last instruction, doesn't do anything, it's just to have a place to put a breakpoint.
     __NOP();
